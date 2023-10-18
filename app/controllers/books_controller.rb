ccbd67bf3d3437ab10c11c144af3e34b22a86814
miyamiyadai <143974725+miyamiyadai@users.noreply.github.com>
2023-10-18 12:04:23 +0000

    first commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,49 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id]) 
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "create successfully"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "create error"
+      render :new 
+    end
+  end 
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "update successfully"
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy 
+    flash[:notice] = "delete successfully"
+    redirect_to '/books' 
+  end
+  
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
